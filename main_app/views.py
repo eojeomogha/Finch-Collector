bf83d5ec607ee1a3efac0f15833d1195fd838442
eojeomogha <emmanuelojeomogha@gmail.com>
2023-07-11 01:17:27 -0700

    Lab 1

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
+finches = [
+  {'name': 'The Finch', 'breed': 'grumpy', 'description': 'Steals finchmas', 'age': 3},
+  {'name': 'Finnochio', 'breed': 'puppet', 'description': 'Wants to be a real finch', 'age': 2},
+]
 # Create your views here.
+def home(request):
+    # Include an .html file extension
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def finches_index(request):
+    return render(request, 'finches/index.html', {
+        'finches': finches
+    })
