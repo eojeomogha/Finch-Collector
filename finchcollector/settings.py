8847c33ab090abb620eb983151a49ec0b878e1dc
eojeomogha <emmanuelojeomogha@gmail.com>
2023-07-12 00:48:04 -0700

    MVT structure

--- finchcollector/settings.py
+++ finchcollector/settings.py
@@ -78,7 +78,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME':'finchcollector',
-        'PASSWORD': 123
+        'USER': 'postgres',
+        'PASSWORD': '123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
--- main_app/views.py
+++ main_app/views.py
@@ -1,18 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Finch
+from .forms import FeedingForm
 
-finches = [
-  {'name': 'The Finch', 'breed': 'grumpy', 'description': 'Steals finchmas', 'age': 3},
-  {'name': 'Finnochio', 'breed': 'puppet', 'description': 'Wants to be a real finch', 'age': 2},
-]
 # Create your views here.
+
+
 def home(request):
-    # Include an .html file extension
     return render(request, 'home.html')
+
 
 def about(request):
     return render(request, 'about.html')
 
+
 def finches_index(request):
+    finches = Finch.objects.all()
+    
     return render(request, 'finches/index.html', {
         'finches': finches
     })
+
+
+def finches_detail(request, finch_id):
+    finch = Finch.objects.get(id=finch_id)
+    feeding_form = FeedingForm()
+    return render(request, 'finches/detail.html', {'finch': finch, 'feeding_form': feeding_form})
+
+
+class FinchCreate(CreateView):
+    model = Finch
+    fields = '__all__'
+
+
+class FinchUpdate(UpdateView):
+    model = Finch
+    
+    fields = ['gender', 'species', 'age']
+
+
+class FinchDelete(DeleteView):
+    model = Finch
+    success_url = '/finches'
+
+
+def add_feeding(request, finch_id):
+    form = FeedingForm(request.POST)
+    if form.is_valid():
+        new_feeding = form.save(commit=False)
+        new_feeding.finch_id = finch_id
+        new_feeding.save()
+    return redirect('detail', finch_id=finch_id)
